4ce852035e0ae89b52eb88c14f6a3aa790c04eaa
shohhen <96072548+shohhen@users.noreply.github.com>
2023-12-19 23:02:50 +0500

    -m

--- src/components/courses/Courses.jsx
+++ src/components/courses/Courses.jsx
@@ -4,48 +4,53 @@ import CustomModal from "../modal/Modal";
 import { useState, useEffect } from "react";
 
 const Courses = () => {
-  const [modals, setModals] = useState([
-    { isOpen: false, content: "Content for Modal 1" },
-    { isOpen: false, content: "Content for Modal 2" },
-    { isOpen: false, content: "Content for Modal 3" },
-    { isOpen: false, content: "Content for Modal 4" },
-    { isOpen: false, content: "Content for Modal 5" },
-    { isOpen: false, content: "Content for Modal 6" },
-    { isOpen: false, content: "Content for Modal 7" },
-    { isOpen: false, content: "Content for Modal 8" },
-  ]);
+  
 
-  useEffect(() => {
-    // Logic for each modal content when opened
-    const updatedModals = modals.map((modal, index) => {
-      if (modal.isOpen) {
-        return { ...modal, content: `Content for Modal ${index + 1}` };
-      }
-      return modal;
-    });
-    setModals(updatedModals);
-  }, []);
+  const [selectedBlock, setSelectedBlock] = useState(null)
 
-  const handleOpenModal = (index) => {
-    const updatedModals = [...modals];
-    updatedModals[index].isOpen = true;
-    setModals(updatedModals);
+  const handleOpenModal = (modalId) => {
+    setSelectedBlock(modalId);
   };
 
-  const handleCloseModal = (index) => {
-    const updatedModals = [...modals];
-    updatedModals[index].isOpen = false;
-    setModals(updatedModals);
+  const handleCloseModal = () => {
+    setSelectedBlock(null);
   };
+
+  const blocks = [
+    { id: 1, img:"/src/assets/img/lab-flask.svg", name: 'Block 1' },
+    { id: 2, img:"/src/assets/img/lab-flask.svg", name: 'Block 2' },
+    { id: 3, img:"/src/assets/img/lab-flask.svg", name: 'Block 1' },
+    { id: 4, img:"/src/assets/img/lab-flask.svg", name: 'Block 2' },
+    { id: 5, img:"/src/assets/img/lab-flask.svg", name: 'Block 1' },
+    { id: 6, img:"/src/assets/img/lab-flask.svg", name: 'Block 2' },
+    { id: 7, img:"/src/assets/img/lab-flask.svg", name: 'Block 1' },
+    { id: 8, img:"/src/assets/img/lab-flask.svg", name: 'Block 2' },
+    // Add more blocks as needed
+  ];
+
+  const modals = [
+    { id: 1, content: 'Content for Modal 1' },
+    { id: 2, content: 'Content for Modal 2' },
+    { id: 3, content: 'Content for Modal 1' },
+    { id: 4, content: 'Content for Modal 2' },
+    { id: 5, content: 'Content for Modal 1' },
+    { id: 6, content: 'Content for Modal 2' },
+    { id: 7, content: 'Content for Modal 1' },
+    { id: 8, content: 'Content for Modal 2' },
+    // Match modal IDs with block IDs or customize as needed
+  ];
+
+
+
 
   const renderModals = () => {
     return modals.map((modal, index) => (
       <CustomModal
-        key={index}
-        isOpen={modal.isOpen}
-        onClose={() => handleCloseModal(index)}
+        key={modal.id}
+        isOpen={selectedBlock === modal.id}
+        onClose={handleCloseModal}
       >
-        <h2>Modal {index + 1}</h2>
+        <h2>Modal {modal.id}</h2>
         <p>{modal.content}</p>
       </CustomModal>
     ));
@@ -54,16 +59,16 @@ const Courses = () => {
   return (
     <div className=" courses main-wrapper" id="courses">
       <div className="main-component">
-        <h2>Explore Topics With Precision</h2>
+        <h2>Explore Courses With Precision</h2>
         <div className="courses__list">
-          {modals.map((modal, index) => (
+          {blocks.map((block) => (
             <div
               className="courses__list-single"
-              key={index}
-              onClick={() => handleOpenModal(index)}
+              key={block.id}
+              onClick={() => handleOpenModal(block.id)}
             >
-              <img src={image} alt="" />
-              <p>optimal nutrition</p>
+              <img src={block.img} alt="" />
+              <p>{block.name}</p>
             </div>
           ))}
           {renderModals()}
